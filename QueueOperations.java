946ef04c35c21c373af193a16cc4f997c5e22aab
Mohit Choudhari <mohitchoudhari162@gmail.com>
2026-03-29 17:00:06 +0530

    queue implementation using linked list

--- QueueOperations.java
+++ QueueOperations.java
@@ -45,11 +45,52 @@ class Node{
     }
 }
 
+class QueueList{
+
+    Node front, rear;
+
+    void enqueue(int val){
+        Node newNode = new Node(val);
+
+        if(rear == null){
+            front = rear = newNode;
+        }
+
+        rear.next = newNode;
+        rear = newNode;
+    }
+
+    int dequeue(){
+
+        if(front == null){
+            System.out.println("underflow");
+        }
+        int val = front.val;
+        front = front.next;
+
+        if(front == null){
+            rear = null;
+        }
+        return val;
+    }
+
+    int peek(){
+        if(front == null) return -1;
+        return front.val;
+    }
+
+    boolean isEmpty(){
+        return front==null;
+    }
+}
+
 public class QueueOperations {
     public static void main(String[] args){
 
         QueueArray arr = new QueueArray(5);
+        QueueList ll = new QueueList();
 
         arr.enqueue(1);
+        ll.enqueue(1);
     }
 }
