57df651c62f3f84b1c20bed4009e5edd44273f2d
Mohit Choudhari <mohitchoudhari162@gmail.com>
2026-03-29 13:28:30 +0530

    Stack implementation using array

--- StackOperations.java
+++ StackOperations.java
@@ -1,26 +1,69 @@
 import java.util.*;
 
-public class StackOperations {
-    
-    public void InbuiltStack(){
+class StackArray {
 
-        Stack<Integer> stack = new Stack<>(); 
-        
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
+    int arr[];
+    int size;
+    int top;
 
-        System.out.println(stack.peek());
-
-        stack.pop();
-
-        System.out.println(stack.peek());
+    StackArray(int size){
+        this.size = size;
+        arr = new int[size];
+        top = -1;
     }
 
+    void push(int val){
+
+        if(top == size - 1){
+            System.out.println("Stack overflow");
+            return;
+        }
+        arr[++top] = val;
+    }
+
+    int pop() {
+
+        if(top == -1){
+            System.out.println("Stack underflow");
+            return -1;
+        }
+        return arr[top--];
+    }
+
+    int peek() {
+
+        if(top == -1){
+            System.out.println("Stack underflow");
+            return -1;
+        }
+        System.out.println(arr[top]);
+        return arr[top];
+    }
+
+    boolean isEmpty(){
+        return top == -1;
+    }
+
+}
+
+public class StackOperations {
     public static void main(String[] args){
 
-        StackOperations st = new StackOperations();
+        // Stack<Integer> stack = new Stack<>(); 
+        
+        // stack.push(10);
+        // stack.push(20);
+        // stack.push(30);
 
-        st.InbuiltStack();
+        // System.out.println(stack.peek());
+
+        // stack.pop();
+
+        // System.out.println(stack.peek());
+
+        StackArray arr = new StackArray(5);
+
+        arr.push(1);
+        arr.peek();
     }
 }
