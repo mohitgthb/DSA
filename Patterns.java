8a17343d0492ee0147ab24143b5e98c41f4468e9
Mohit Choudhari <mohitchoudhari162@gmail.com>
2026-04-28 18:14:42 +0530

    number patterns

--- Patterns.java
+++ Patterns.java
@@ -19,11 +19,31 @@ class Patterns {
         }
         System.out.println();
     }
+
+    void pattern3(){
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+      void pattern4(){
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         
         Patterns obj = new Patterns();
 
         obj.pattern1();
         obj.pattern2();
+        obj.pattern3();
+        obj.pattern4();
     }
 }
