39e6c4452a4777665c7a41a5be7bc9fb699d3776
Mohit Choudhari <mohitchoudhari162@gmail.com>
2026-04-28 18:57:39 +0530

    More advanced patterns

--- Patterns.java
+++ Patterns.java
@@ -37,13 +37,66 @@ class Patterns {
             System.out.println();
         }
     }
+
+    void pattern5(){
+        for(int i=5; i>0; i--){
+            for(int j=0; j<i; j++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    void pattern6(){
+        for(int i=5; i>0; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print(j+ " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    void pattern7(int N){
+        for(int i=0; i<N; i++){
+            for(int j=0; j< N-i-1; j++){
+                System.out.print( "  ");
+            }
+
+            for(int j=0; j<2*i + 1; j++){
+                System.out.print("*" + " ");
+            }
+
+            for(int j=0; j< N-i-1; j++){
+                System.out.print( "  ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    void pattern8(int N){
+        for(int i=0; i<N; i++){
+            for(int j=0; j<i; j++){
+                System.out.print( "  ");
+            }
+
+            for(int j=0; j<2*N - (2*i + 1); j++){
+                System.out.print("*" + " ");
+            }
+
+            for(int j=0; j< i; j++){
+                System.out.print( "  ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         
         Patterns obj = new Patterns();
 
-        obj.pattern1();
-        obj.pattern2();
-        obj.pattern3();
-        obj.pattern4();
+        obj.pattern8(5);
     }
 }
