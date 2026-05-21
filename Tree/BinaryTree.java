c868871c67922d067cf1f7c612d7b19e9e1ceee9
Mohit Choudhari <mohitchoudhari162@gmail.com>
2026-05-21 17:49:10 +0530

    Postorder traversal with one stack and two stack

--- Tree/BinaryTree.java
+++ Tree/BinaryTree.java
@@ -57,6 +57,55 @@ public class BinaryTree {
         }
     }
 
+    public void postorder2stack(Node root){
+        if(root == null) return;
+
+        Stack<Node> st1 = new Stack<>();
+        Stack<Node> st2 = new Stack<>();
+
+        st1.push(root);
+
+        while(!st1.isEmpty()){
+            Node node = st1.pop();
+            st2.push(node);
+
+            if(node.left != null){
+                st2.push(node.left);
+            }
+            if(node.right != null){
+                st2.push(node.right);
+            }
+        }
+        while(!st2.isEmpty()){
+           System.out.print(st2.pop() + " ");
+        }
+        
+    }
+
+    public void postorder1stack(Node root){
+        if(root == null) return;
+
+        Stack<Node> st = new Stack<>();
+        Node curr = root;
+        Node lastVisited = null;
+
+        while(curr != null || !st.isEmpty()){
+            
+            while(curr != null){
+                st.push(curr);
+                curr = curr.left;
+            }
+            Node peekNode = st.peek();
+
+            if(peekNode.right != null && lastVisited != peekNode.right){
+                curr = peekNode.right;
+            } else {
+                System.out.print(peekNode.val + " ");
+                lastVisited = st.pop();
+            }
+        }
+    }
+
     public static void main(String[] args){
         BinaryTree tree = new BinaryTree();
 
